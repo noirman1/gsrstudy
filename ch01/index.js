fc0df177fd477b78a15a323dd3c9ca0f08ef654f
noirman1 <noirman@naver.com>
2018-11-28 13:59:28 +0900

    .

--- ch01/index.js
+++ ch01/index.js
@@ -3,9 +3,9 @@ var result = 0;
 console.time('time check');
 
 for(var i=0; i<=10000; i++){
-    result += 1;
+    result += i;
+    
 }
-
+console.log("result= %d",result);
 console.timeEnd('time Check');
 
-console.log("result= %d",result);
